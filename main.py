ab5ec2b2f69721d3ee38fda8e8b913f0fef9fc18
1King-coder <progvitor@gmail.com>
2024-08-11 16:18:07 -0300

    deleted init_api

--- main.py
+++ main.py
@@ -4,25 +4,7 @@ from pages.Home import HomePage
 import pyautogui as pg
 from time import sleep
 
-def init_api ():
-    pg.keyDown('win')
-    pg.press('r')
-    pg.keyUp('win')
 
-    pg.write('cmd')
-    pg.press('enter')
-    sleep(0.5)
-    pg.write('g:')
-    pg.press('enter')
-    pg.write('cd Meu Drive\\Controle Financeiro\\backend')
-    pg.press('enter')
-    pg.write('backendEnv\\Scripts\\activate')
-    pg.press('enter')
-    pg.write('uvicorn main:app')
-    pg.press('enter')
-
-
-# init_api()
 
 app = ctk.CTk(fg_color=colors['color1'])
 
